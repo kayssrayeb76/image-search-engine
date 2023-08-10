2a37fe027303d8ffd3daadb02d3c5ba1309d54f5
kayssrayeb76 <kayssrayeb76@gmail.com>
2023-08-10 22:14:17 +0100

    add main files

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const searchFrom = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+const accessKey = "Wv4BCWQ6Tfmxa2GUEu1LAtZBuQOptDxI2h77aPHQpZw";
+
+// access key : Wv4BCWQ6Tfmxa2GUEu1LAtZBuQOptDxI2h77aPHQpZw
+// secret key : Sro-XZVFAWisVQ3RI_rfn-auJstO4fOq-FsVrtwmaS0
+
+let keyword = "";
+let page = 1;
+
+async function searchImage() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((res) => {
+        const image = document.createElement('img');
+        image.src = res.urls.small;
+        const imageLink = document.createElement('a');
+        imageLink.href = res.links.html;
+        imageLink.target = '_blank';
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
+
+    showMoreBtn.style.display = 'block';
+}
+
+searchFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImage();
+});
+
+showMoreBtn.addEventListener('click', () => {
+    page++;
+    searchImage();
+});
